8a790ff8bb6b49218c0f5cbbb1b437452d62ec78
SEYA-CODING <seyattv@gmail.com>
2025-10-16 23:59:50 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+-- =========================================================================
+-- ScriptDoctor
+-- Created by: SEYA-CODING
+-- Discord: seya_coding #6497
+-- Community: https://discord.gg/RT3uJRdXSC
+-- Note: All rights reserved. Redistribution or resale without permission is prohibited.
+-- =========================================================================
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SEYA-CODING'
+description 'ScriptDoctor - Automatic fixer and helper for ESX and QBCore scripts (hybrid)'
+version '1.2.0'
+
+server_script 'server/main.lua'
+client_script 'client/main.lua'
+shared_script 'config.lua'
+
+files {}
